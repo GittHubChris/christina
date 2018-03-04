0e9bb90688b98a7fbc8192a09959577292d5fe7f
GittHubChris <36934039+GittHubChris@users.noreply.github.com>
2018-03-04 23:27:16 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,11 @@
-
+//search.js
+$(function (Query){ 'use strict';
+                   var query=new Query();
+                   $('.search').on ('submit',submit);
+                   function submit(e) {
+                     //stop the form from doing its default bahavior
+                     e.preventDefault();
+                     //set the query and go to the search page with our query URL
+                     .set($('.search-box').val().trim()).goToLocation('/search');
+                   }
+                  }(Query));
