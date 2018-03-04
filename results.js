30d14536ffc8ebc08c6850f2257c3af017e8dc37
GittHubChris <36934039+GittHubChris@users.noreply.github.com>
2018-03-04 23:39:43 +0200

    Update results.js

--- results.js
+++ results.js
@@ -1,1 +1,16 @@
+//We will have the following code on our search page
+//results.js
+$(function(Query,utils){
+  var query = new Query(),
+        site = location.protocol + "//" + location.host,
+        // some utility functions
+        utils = utils;
 
+    query
+    .setFromURL('query')
+    .getJSON('/data.json')
+    .done(function(data) {
+        console.log(data);
+        // show our results
+    });    
+}(Query,utils));
